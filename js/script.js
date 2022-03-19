8e9813d28a9e6ea726bfc8950315dc470436c78f
ElenaDutova <eluvarova0997@mail.ru>
2022-03-19 17:23:29 +0300

    Practice p.3. Functions
    Lesson 18

--- js/script.js
+++ js/script.js
@@ -1,42 +1,95 @@
 "use strict";
 
-/* Object prototypes */
+/* Lesson 18. Practice p.3. Functions */
 
-// const str = "something";
-// const strObj = new String(str);
+/* Задание на урок:
 
-// console.log(typeof(str));
-// console.log(typeof(strObj));
+1) Первую часть задания повторить по уроку
 
-// console.dir([1, 3, 5, 7]);
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
 
-const soldier = {
-    health: 200,
-    mana: 200,
-    attack: 5,
-    armor: 7,
-    weapon: true,
-    sayHello: function() {
-        console.log('Hello!');
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно */
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt("Сколько фильмов Вы уже посмотрели?", "");
     }
+}
+
+start();
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
 };
 
-const johnKonstantin = {
-    health: 300
-};
+
+function rememberMyFilms() {
+    for ( let i = 0; i < 2; i++) {
+        const a = prompt("Один из последних просмотренных фильмов?", ""),
+              b = prompt("На сколько оцените его?", "");
+        
+        if (a != null && b != null && a != "" && b != "" && a.length < 50 && b.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log("Done!");
+        } else {
+            console.log("NO!");
+            i--;
+        }
+    }
+}
+
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("Просмотрено довольно мало фильмов");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("Вы классический зритель");
+    } else if (personalMovieDB.count >= 30) {
+        console.log("Вы киноман");
+    } else {
+        console.log("Произошла ошибка");
+    }
+}
+
+detectPersonalLevel();
+
+function writeYourGenres() {
+    for (let i = 0; i < 3; i++) {
+        personalMovieDB.genres[i] = prompt(`Ваш любимый жанр под номером ${i+1}`, "");
+        if (personalMovieDB.genres[i] == '' || personalMovieDB.genres[i] == null) {
+            i--;
+        }
+    }
+}
+
+writeYourGenres();
+
+function showMyDB() {
+    if (!personalMovieDB.privat) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB();
 
 
-// johnKonstantin.__proto__ = soldier;
-// console.log(johnKonstantin.armor);
-// johnKonstantin.sayHello();
 
-// Object.setPrototypeOf(johnKonstantin, soldier);
-// console.log(johnKonstantin.armor);
-// johnKonstantin.sayHello();
 
-const vasya = Object.create(soldier);
 
-console.log(vasya);
-console.log(vasya.attack);
+
 
 
