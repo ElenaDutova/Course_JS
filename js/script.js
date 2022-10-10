545ce028a9a7297903ac7f2badcea32dface2ba6
ElenaDutova <eluvarova0997@mail.ru>
2022-10-10 18:18:49 +0300

    Date()

--- js/script.js
+++ js/script.js
@@ -1,104 +1,20 @@
 'use strict';
 
+const now = new Date();
 
-const btn = document.querySelector('.btn');
+// console.log(now.getFullYear());
+// console.log(now.getMonth());
+// console.log(now.getDate());
+// console.log(now.getDay());
+// console.log(now.getUTCHours());
+console.log(now.getTimezoneOffset());
 
-let timerID,
-    i = 0;
+let start = new Date();
 
-
-// Глобальная функция setTimeout() позволяет запустить функцию через определенный промежуток времени:
-
-// 1
-
-// const timerID = setTimeout(function() {
-//     console.log('HELLO');
-// }, 2000);
-
-
-// 2
-
-// const timerID = setTimeout(function(text) {
-//     console.log(text);
-// }, 2000, 'HELLO');
-
-
-
-// 3
-
-// const timerID = setTimeout(logger, 2000);
-
-// function logger() {
-//     console.log('text');
-// }
-
-
-// 4
-// Чтобы удалить setTimeout мы можем использовать 
-
-// clearTimeout(timerID);
-// clearInterval(timerID);
-
-
-// 5
-
-// btn.addEventListener('click', () => {
-//     const timerID = setTimeout(logger, 2000);
-// });
-
-
-
-// function logger() {
-//     console.log('text');
-// }
-
-
-// Глобальная функция setInterval() позволяет вызывать функцию снова и снова через определенный промежуток времени:
-
-// 1
-
-// btn.addEventListener('click', () => {
-//     timerID = setInterval(logger, 1500);
-// });
-
-
-
-// function logger() {
-//     if (i === 3) {
-//         clearInterval(timerID);
-//     }
-//     console.log('text');
-//     i++;
-// }
-
-
-
-// Вызов рекурсивного setTimeout
-
-
-// let id = setTimeout(function log() {
-//     console.log("yellow");
-//     id = setTimeout(log, 500);
-// }, 500);
-
-
-// Создаем анимацию:
-
-function myAnimation() {
-    const elem = document.querySelector('.box');
-    let position = 0;
-
-    const id = setInterval(frame, 10);
-    function frame () {
-        if (position == 300) {
-            clearInterval(id);
-        } else {
-            position++;
-            elem.style.top = position + 'px';
-            elem.style.left = position + 'px';
-        }
-    }
+for (let i = 0; i < 1000000; i++) {
+    let some = i ** i;
 }
 
+let end = new Date();
 
-btn.addEventListener('click', myAnimation);
+alert(`Цмкл отработал за ${end - start} миллисекунд`);
